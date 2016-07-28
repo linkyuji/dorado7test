dd6e795dda1036cd15073c3b0505fd9dcb20021b
linkyuji <122461757@qq.com>
2016-07-28 17:09:38 +0800

    单参数查询，多参数查询

--- quick-start/src/sample/chapter05/SimpleCRUD.java
+++ quick-start/src/sample/chapter05/SimpleCRUD.java
@@ -1,9 +1,11 @@
 package sample.chapter05;
 
 import java.util.Collection;
+import java.util.Map;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -34,6 +36,37 @@ public class SimpleCRUD {
     @Transactional
     public void saveAll(Collection<Product> products){
         productDao.persistEntities(products);
+    }
+    
+    @DataProvider
+    public void query1(Page<Product> page, String productName) {
+        if (null != productName)
+            productDao.find(page, "from Product where productName like '%"
+                    + productName + "%'");
+        else
+            productDao.getAll(page);
+    }
+    
+    @DataProvider
+    public void query2(Page<Product> page, Map<String, Object> params) {
+        if (null != params) {
+            String productName = (String) params.get("productName");
+            String unitsInStock = (String) params.get("unitsInStock");
+
+            String whereCase = "";
+            if (null != productName) {
+                whereCase += " productName like '%" + productName + "%' ";
+            }
+            if (null != unitsInStock) {
+                if (StringUtils.isNotEmpty(whereCase))
+                    whereCase += " AND ";
+ 
+                whereCase += " unitsInStock >" + unitsInStock;
+            }
+            String queryStr = (StringUtils.isEmpty(whereCase)) ? "" : " where "+ whereCase;
+            productDao.find(page, " from Product " + queryStr);
+        } else
+            productDao.getAll(page);
     }
 
 }
